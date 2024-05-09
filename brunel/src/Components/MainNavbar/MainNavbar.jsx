1b524dd3f5837335d3fdca2b5b2eecad2e4e4921
Parth Puranik <thosparth@gmail.com>
2024-05-09 15:54:16 +0530

    hero done

--- brunel/src/Components/MainNavbar/MainNavbar.jsx
+++ brunel/src/Components/MainNavbar/MainNavbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import './MainNavbar.css'
-import burnelLogo from '../../assets/burnelLogo.png'
+import React from "react";
+import "./MainNavbar.css";
+import burnelLogo from "../../assets/burnelLogo.png";
 
 export default function MainNavbar() {
   return (
-    <div className='mainNavbarDiv'>
-      <div className="mainNavbarRect"><img className="burnelLogo" src={burnelLogo} alt="" /><div className="mainNavCta"><button className='getProjectsBtn'>Get Project</button><button>hi</button></div></div>
+    <div className="mainNavbarDiv">
+      <div className="mainNavbarRect">
+        <img className="burnelLogo" src={burnelLogo} alt="" />
+        <div className="mainNavCta">
+          <button className="getProjectsBtn">Get Projects</button>
+          <button className="onboardBtn">OnBoard Talent</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- brunel/src/Pages/Home/Home.jsx
+++ brunel/src/Pages/Home/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import './Home.css'
-import MainNavbar from '../../Components/MainNavbar/MainNavbar'
+import React from "react";
+import "./Home.css";
+import MainNavbar from "../../Components/MainNavbar/MainNavbar";
 
 export default function Home() {
   return (
     <div>
       <MainNavbar />
+      <div className="heroSect">
+        <div className="heroHead">
+          <div className="graceText">Success Stories</div>
+          <div className="heroHeadMain">Every success journey we've encountered.</div>
+        </div>
+
+        <div className="subHeroDiv">
+          <div className="subHeroStats"></div>
+          <div className="subHeroExplore"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
