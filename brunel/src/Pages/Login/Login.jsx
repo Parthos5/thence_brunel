c9057721c69f2232e08955e20fe7b2c4457b0903
Parth Puranik <thosparth@gmail.com>
2024-05-09 19:38:16 +0530

    done

--- brunel/src/Pages/Login/Login.jsx
+++ brunel/src/Pages/Login/Login.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import burnelLogo from "../../assets/burnelLogo.png";
+import crossIcon from "../../assets/crossIcon.png";
 
 export default function Login() {
   return (
     <div>
-      
+      <div className="loginHead">
+        <img src={burnelLogo} className="burnelLogo2" alt="" />
+        <div className="crossIconDiv">
+          <img src={crossIcon} className="crossIcon" alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
